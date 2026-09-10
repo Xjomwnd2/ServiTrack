9aedf23d377e4b2ca9acfe6a6e22f7b63818dac0
Joel Ndiba Mwaura <180766891+Xjomwnd2@users.noreply.github.com>
2026-09-10 20:07:22 +0300

    commit

--- server/routes/jobRoutes.js
+++ server/routes/jobRoutes.js
@@ -1,1 +1,27 @@
-cat routes/jobRoutes.js
+const express = require("express");
+
+const {
+  addJob,
+  listJobs,
+  getJob,
+  editJob,
+  changeJobStatus,
+  listJobHistory,
+  removeJob,
+} = require("../controllers/jobController");
+
+const authenticateToken = require("../middleware/authMiddleware");
+
+const router = express.Router();
+
+router.use(authenticateToken);
+
+router.post("/", addJob);
+router.get("/", listJobs);
+router.get("/:id", getJob);
+router.get("/:id/history", listJobHistory);
+router.put("/:id", editJob);
+router.patch("/:id/status", changeJobStatus);
+router.delete("/:id", removeJob);
+
+module.exports = router;
